133178bb8eb5d592a9daab1ade55af98ca6c1910
Tudor Gheorghiu <tudi.gheorghiu@gmail.com>
2023-05-21 23:36:14 +0300

    Updates

--- src/script.js
+++ src/script.js
@@ -1,13 +1,10 @@
-// const navLinks = document.querySelectorAll('.nav-item')
-// const menuToggle = document.getElementById('navbarSupportedContent')
-// const bsCollapse = new bootstrap.Collapse(menuToggle)
 
-// addEventListener('click', () => {
-//     bsCollapse.style.backgroundColor = "#FFF";
-// })
+
 
 const navbar = document.getElementsByName("nav");
 
 function navBackground() {
     navbar.style.backgroundColor = "#FFF";
+    alert("AAA");
 }
+
